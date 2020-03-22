b8fc534a7dd24eb5a90be61a6b2d2cd533d85cde
Henrick Mello <hmartins505@gmail.com>
2020-03-22 20:19:30 -0300

    Fixed projects rounded corners

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,19 +1,17 @@
 export default function Project({ title, description, image, link }) {
   return (
     <a href={link} target="_blank" rel="noopener">
-      <div className="relative mx-4 mb-6 text-center shadow-2xl mx:px-0">
+      <div className="relative mx-4 mb-6 overflow-hidden text-center rounded shadow-2xl mx:px-0 ">
         <img
           src={image}
-          className="w-full h-auto rounded object-fit"
+          className="w-full min-h-full object-fit"
           alt={"Imagem do projeto " + title}
         />
         <div
           className="absolute bottom-0 w-full p-4"
           style={{
             backdropFilter: "blur(8px)",
-            backgroundColor: "rgba(5,5,5,0.5)",
-            borderBottomLeftRadius: "0.25rem",
-            borderBottomRightRadius: "0.25rem"
+            backgroundColor: "rgba(5,5,5,0.5)"
           }}
         >
           <h2 className="text-xl font-semibold text-gray-100 md:text-3xl">
